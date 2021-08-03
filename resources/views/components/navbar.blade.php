cf5521e1cae92acdc84d66e53bd2fd09c5f64bda
Kenneth Abenojar <abenojargup@Kenneths-MacBook-Pro.local>
2021-08-03 14:47:25 +0800

    feat: edited logo and app name

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-warning">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img width="40px" src="https://png.pngtree.com/png-vector/20190223/ourmid/pngtree-shipping-logistics-logo-vector-illustration-png-image_691258.jpg" alt=""> 
+      <b>
+        <i>
+          LOGISTICS SOFTWARE
+        </i>
+      </b>
+    </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown"></div>
+    
+    <div class="d-flex">
+      <ul class="navbar-nav"> 
+        <li class="nav-item dropdown">
+          <a class="text-white nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            John Doe 
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">My Profile</a></li> 
+            <li><a class="dropdown-item" href="#">Logout</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
